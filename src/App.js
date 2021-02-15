0164fe780062e05e36ff54ad598379981fd8407c
Jeremiah Sifuentes <jerrysifuentes01@gmail.com>
2021-02-15 13:03:04 -0600

    Adding responsive background Images

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
+import background from '../src/images/BackgroundImage.jpg';
+import backgroundTablet from '../src/images/Tablets.jpg';
 
 function App() {
+
+  const imageUrl = window.innerWidth >= 650 ? backgroundTablet : background;
+
   return (
-    <div className='backgroundImage'>
-      <div className='title-text'>
-      <h1 className='top'>Hells</h1>
-        <h1>Paradise</h1>
-        <h1 className='bottom'>Nails</h1>
+    <div className='container'>
+      <div className='landing-wrapper' style={{
+        backgroundImage: `url(${imageUrl})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        }}>
+      
       </div>
     </div>
   );
 }
 
 export default App;
+
+// // <div className='title-text'>
+// {/* <h1 className='top'>Hells</h1>
+// <h1>Paradise</h1>
+// <h1 className='bottom'>Nails</h1>
+// </div> */}
